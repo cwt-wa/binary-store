cde42e659411731145d28456ba0d515adea40cf5
Zemke <florian@zemke.io>
2019-08-27 18:49:24 +0200

    REST interface

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,55 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class UserMatchingCallback
+{
+    private $userId;
+
+    public function __construct($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function __invoke($it)
+    {
+        return $it->getBasename('.' . $it->getExtension()) === $this->userId;
+    }
+};
+
+function findMatchingUserPics($userId) {
+    return array_filter(
+            File::files("../binary/photo/"),
+            new UserMatchingCallback($userId));
+}
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+
+    $router->get('user/{userId}/photo', function ($userId) {
+        $files = findMatchingUserPics($userId);
+        if (empty($files)) {
+            $rndFiles = File::files("./albino");
+            $rnd = rand(0, count($rndFiles) - 1);
+            Log::debug($rndFiles[$rnd]->getPathname());
+            return response()->download($rndFiles[$rnd]->getPathname());
+        }
+        return response()->download("../binary/photo/" . $files[0]->getFilename());
+    });
+
+    $router->post('user/{id}/photo', function (Request $request, $userId) {
+        $currFiles = array_map(function ($it) {
+            return $it->getPathname();
+        }, findMatchingUserPics($userId));
+        File::delete($currFiles);
+        $uploadedFile = $request->file('photo');
+        $uploadedFile->move(
+            '../binary/photo',
+            "$userId." . $uploadedFile->extension());
+    });
+});
+
